8aa20251eee82f7deabd4d6c02064a2cad2966c8
Cchenbin <32832234+Cchenbin@users.noreply.github.com>
2019-03-29 19:13:43 +0800

    添加GitTest到工程

---
+++ git/src/main/java/com/chen/GitTest.java
@@ -0,0 +1,13 @@
+package com.chen;
+
+/**
+ * @author: ChenBin
+ * @date: 2019/3/29 18:47
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111111111111111111111");
+    }
+
+}
